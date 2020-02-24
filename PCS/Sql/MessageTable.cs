c962ff1c24be0ecd0b9931d10a29136fbdd177c2
paulwacquet <paulwacquet@outlook.com>
2020-02-24 15:45:14 +0100

    test

---
+++ PCS/Sql/MessageTable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace PCS.Sql
+{
+    public class MessageTable : Table<BroadcastMessage>
+    {
+        protected override string Name => "Messages";
+
+        protected override Dictionary<string, object> GetValues(BroadcastMessage broadcast)
+        {
+            if (broadcast == null)
+                throw new ArgumentNullException(nameof(broadcast));
+
+            return new Dictionary<string, object>()
+            {
+                { "Id", broadcast.ID },
+                { "Text", broadcast.BaseMessage.Text },
+                { "Channel", broadcast.BaseMessage.ChannelName },
+                { "AuthorId", broadcast.Author.ID },
+                { "DateTime", broadcast.DateTime }
+            };
+        }
+
+        protected override SqlParameter[] GetParameters()
+            => new[]
+            {
+                new SqlParameter() { ParameterName = "Id", SqlDbType = SqlDbType.Int, IsNullable = false },
+                new SqlParameter() { ParameterName = "Text", SqlDbType = SqlDbType.NText, IsNullable = false },
+                new SqlParameter() { ParameterName = "Channel", SqlDbType = SqlDbType.NChar, Size = 50, IsNullable = false },
+                new SqlParameter() { ParameterName = "AuthorId", SqlDbType = SqlDbType.Int, IsNullable = false },
+                new SqlParameter() { ParameterName = "DateTime", SqlDbType = SqlDbType.DateTime, IsNullable = false }
+            };
+    }
+}
---
+++ PCS/Sql/Table.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Globalization;
+
+namespace PCS.Sql
+{
+    public abstract class Table<T>
+    {
+        public Table()
+        {
+            Database.TryCreate();
+            TryCreate(); // The table
+        }
+
+        protected abstract string Name { get; }
+
+        public void AddRow(T rowObj)
+        {
+            var values = GetValues(rowObj);
+
+            string cmdText = string.Format(CultureInfo.InvariantCulture, "INSERT INTO {0} ({1}) Values ({2})",
+                Name,
+                string.Join(",", values.Keys),
+                string.Join(",", from value in values select '@' + value.Key ?? throw new NullReferenceException())
+                );
+
+            using (SqlConnection conn = new SqlConnection($"server=;Initial Catalog = {Database.Name};Integrated security=SSPI"))
+            using (var cmd = new SqlCommand())
+            {
+                cmd.Connection = conn;
+
+                foreach (var value in values)
+                    cmd.Parameters.AddWithValue('@' + value.Key, value.Value ?? throw new NullReferenceException()); // TODO Specify NRException
+
+                cmd.CommandText = cmdText;
+
+                conn.Open();
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+
+
+        protected abstract Dictionary<string, object> GetValues(T item);
+        protected abstract SqlParameter[] GetParameters();
+
+        private void TryCreate()
+        {
+            string cmdTxt = $"IF OBJECT_ID('{Name}') IS NULL " +
+                                $"CREATE TABLE [dbo].[{Name}](";
+
+            var parameters = GetParameters();
+            int i = 0;
+            foreach (var parameter in parameters)
+            {
+                cmdTxt += string.Format(CultureInfo.InvariantCulture,
+                    "[{0}] {1} {2} {3}",
+                    parameter.ParameterName,
+                    parameter.SqlDbType.ToString().ToUpper(CultureInfo.InvariantCulture) + (parameter.Size != 0 ? "(" + parameter.Size + ")" : ""),
+                    parameter.IsNullable ? "NULL" : "NOT NULL",
+                    i == 0 ? "PRIMARY KEY" : "");
+
+                i++;
+                if (i != parameters.Length)
+                    cmdTxt += ',';
+            }
+            cmdTxt += ")";
+
+            using (SqlConnection conn = new SqlConnection($"server=;Initial Catalog = {Database.Name};Integrated security=SSPI"))
+            using (var cmd = new SqlCommand(cmdTxt, conn))
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        private static class Database
+        {
+            public const string Name = "PcsDatabase";
+
+            public static void TryCreate()
+            {
+                const int dataSize = 2,
+                    logSize = 1;
+                const int dataMaxSize = 4000,
+                    logMaxSize = 2000;
+                string todayStr = string.Concat(DateTime.Today.Day.ToString(CultureInfo.InvariantCulture).PadLeft(2, '0'), DateTime.Today.Month.ToString(CultureInfo.InvariantCulture).PadLeft(2, '0'), DateTime.Today.Year.ToString(CultureInfo.InvariantCulture)).PadLeft(4, '0');
+                
+                string cmdTxt = $"IF DB_ID('{Name}') IS NULL " +
+                    $"CREATE DATABASE {Name} ON PRIMARY" +
+                    $"(NAME = {Name}_Data, " +
+                    $"FILENAME = '{Path.GetFullPath(Name)}_{todayStr}.mdf', " +
+                    $"SIZE = {dataSize}MB, MAXSIZE = {dataMaxSize}MB, FILEGROWTH = 10%) " +
+                    $"LOG ON (NAME = {Name}_Log," +
+                    $"FILENAME = '{Path.GetFullPath(Name)}_{todayStr}.ldf'," +
+                    $"SIZE = {logSize}MB, " +
+                    $"MAXSIZE = {logMaxSize}MB, " +
+                    "FILEGROWTH = 10%)"; // DOLATER: Make the limit never buggy
+
+                using (SqlConnection conn = new SqlConnection("server=;Integrated security=SSPI"))
+                using (var cmd = new SqlCommand(cmdTxt, conn))
+                {
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
