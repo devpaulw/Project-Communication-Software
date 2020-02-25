b2eeefc47a7f04f842a091447647edea4dee5012
paulwacquet <paulwacquet@outlook.com>
2020-02-25 15:49:13 +0100

    Namespaces names specified

--- PCS/Data/ControlChars.cs
+++ PCS/Data/ControlChars.cs
@@ -6,7 +6,7 @@ namespace PCS.Data
 {
     static class ControlChars
     {
-        public const char StartOfText = (char)2;
+        public const char StartOfText = (char)2; 
         public const char EndOfText = (char)3;
         public const char EndOfTransmission = (char)4;
         public const char EndOfTransBlock = (char)23;
